ce34c6be32b435df29f82b98f11842945fa47c75
van <vanik.121290@gmail.com>
2023-06-01 23:40:01 +0400

    create asteroid prefabs and the catalog

--- Assets/Scripts/RovioAsteroids/AsteroidType.cs
+++ Assets/Scripts/RovioAsteroids/AsteroidType.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AsteroidType : MonoBehaviour
+public enum AsteroidType 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Small = 0,
+    Medium = 1,
+    Big = 2
 }
